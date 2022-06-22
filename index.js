84ef57173461e47dbc3e4f6034878bdd572b8245
CodeMarshal007 <102981590+CodeMarshal007@users.noreply.github.com>
2022-06-21 18:10:36 -0700

    commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("My name is Abdulmajeed Adeniyi Yusuf")
+console.log("I am 1.78m tall")
+console.log("my country's name is Nigeria")
+
+document.write("<p>My name is Abdulmajeed Adeniyi Yusuf</p>")
+document.write("<p>I am 1.78m tall</p>")
+document.write("<p>My country's name is Nigeria</p>")
